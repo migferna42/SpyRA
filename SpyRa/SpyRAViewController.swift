c90621f16105138a0591ede2ebe7e1967560b38c
Miguel Ángel Fernández Carrillo <migferna@student.42madrid.com>
2020-07-31 20:20:26 +0200

    Finished

--- SpyRa/SpyRAViewController.swift
+++ SpyRa/SpyRAViewController.swift
@@ -9,7 +9,14 @@ import UIKit
 import ARKit
 
 class SpyRAViewController: UIViewController {
-
+    let noseOptions = ["👃", "🐽", "💧", " "]
+    let eyeOptions = ["👁", "🌕", "🌟", "🔥", "⚽️", "🔎", " "]
+    let mouthOptions = ["👄", "👅", "❤️", " "]
+    let hatOptions = ["🎓", "🎩", "🧢", "⛑", "👒", " "]
+    
+    let features = ["nose", "leftEye", "rightEye", "mouth", "hat"]
+    let featureIndices = [[9], [1064], [42], [24, 25], [20]]
+    
     @IBOutlet var sceneView: ARSCNView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,20 +39,65 @@ class SpyRAViewController: UIViewController {
             
       sceneView.session.pause()
     }
+    
+    func updateFeatures(for node: SCNNode, using anchor: ARFaceAnchor) {
+        for (feature, indices) in zip(features, featureIndices)  {
+          let child = node.childNode(withName: feature, recursively: false) as? EmojiNode
+          
+          let vertices = indices.map { anchor.geometry.vertices[$0] }
+          child?.updatePosition(for: vertices)
+        }
+    }
+    @IBAction func handleTao(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: sceneView)
+
+        let results = sceneView.hitTest(location, options: nil)
+
+        if let result = results.first,
+          let node = result.node as? EmojiNode {
+          
+          node.next()
+        }
+    }
 }
 
 extension SpyRAViewController: ARSCNViewDelegate {
     func renderer(
         _ renderer: SCNSceneRenderer,
         nodeFor anchor: ARAnchor) -> SCNNode? {
-            guard let device = sceneView.device else {
-                return nil
+        guard let faceAnchor = anchor as? ARFaceAnchor,
+          let device = sceneView.device else {
+          return nil
         }
         
         let faceGeometry = ARSCNFaceGeometry(device: device)
         let node = SCNNode(geometry: faceGeometry)
     
         node.geometry?.firstMaterial?.fillMode = .lines
+        node.geometry?.firstMaterial?.transparency = 0.0
+
+        let noseNode = EmojiNode(with: noseOptions)
+        noseNode.name = "nose"
+        node.addChildNode(noseNode)
+
+        let leftEyeNode = EmojiNode(with: eyeOptions)
+        leftEyeNode.name = "leftEye"
+        leftEyeNode.rotation = SCNVector4(0, 1, 0, GLKMathDegreesToRadians(180.0))
+        node.addChildNode(leftEyeNode)
+            
+        let rightEyeNode = EmojiNode(with: eyeOptions)
+        rightEyeNode.name = "rightEye"
+        node.addChildNode(rightEyeNode)
+            
+        let mouthNode = EmojiNode(with: mouthOptions)
+        mouthNode.name = "mouth"
+        node.addChildNode(mouthNode)
+            
+        let hatNode = EmojiNode(with: hatOptions)
+        hatNode.name = "hat"
+        node.addChildNode(hatNode)
+
+        updateFeatures(for: node, using: faceAnchor)
     
         return node
     }
@@ -59,7 +111,7 @@ extension SpyRAViewController: ARSCNViewDelegate {
         let faceGeometry = node.geometry as? ARSCNFaceGeometry else {
           return
       }
-        
+    updateFeatures(for: node, using: faceAnchor)
       faceGeometry.update(from: faceAnchor.geometry)
     }
 }
